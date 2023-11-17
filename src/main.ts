f5ee34c50cae169e8ad2ad87a18be975d6bcaae8
Siddhant Kevit <siddhant_prajapati@kevit.io>
2023-11-17 15:52:16 +0530

    adding whitelist : true in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
+
+app.useGlobalPipes(
+  new ValidationPipe({
+    whitelist: true,
+  }),
+);
   await app.listen(process.env.PORT);
 }
 bootstrap();
